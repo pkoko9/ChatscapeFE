923d705b24ff5d2e6c1038896d0625ca8ff2666e
Paul Kokoszyna <pkoko9@hotmail.com>
2015-01-30 19:36:59 -0600

    Chatscape Update Friday

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,25 +2,54 @@
 $(document).ready(onReady); 
 
 function onReady() {
-$('#userSubmit').on('click', onSubmitButtonClick);
+	$('#userSubmit').on('click', onSubmitButtonClick);
+	// $('#getMessages').on('click', getMessages);
 
-function onSubmitButtonClick() {
 
-$.post(
-'https://chatscape.herokuapp.com/chats', //something is wrong with the server URL it's not allowing
-{
-chat: {
+	function onSubmitButtonClick() {
+		
+		$.post(
+		'https://chatscape.herokuapp.com/chats.json', 
+		{
+			chat: {
 
-user: $('#userCreate').val(),
+				user: $('#userCreate').val(),
 
-message: $('#message').val(),
+				message: $('#message').val(),
+
+				created_at: $('').val(),
+			}
+		},
+			messageDisplay(message),
+			getMessages(),
+			'json'
+			);
+	}
+
+	function messageDisplay(messages){
+		console.log (messages);
+		for (var i=0; i<messages.length; i++){
+			$('.messagePost').prepend('<div>User: ' + messages[i].user +'</div><div>Message: '+ messages[i].message + '</div><div>Time: '+ messages[i].created_at + '</div>');
+			}
+		}
+	function getMessages() {
+		// $('#getMessages').on('click', ongetMessagesButtonClick);
+
+		// function ongetMessagesButtonClick () {
+
+			$.get(
+			'https://chatscape.herokuapp.com/chats.json',
+
+			function(message) {
+				console.log(message)
+				messageDisplay(message);
+			},
+			'json'
+			);
+		// }
+
+	}
+	// setInterval(getMessages,1000000);
 }
-},
-function(message) {
-console.log(message)
-},
-'json'
-);
-}
-}
+
 
